6068f32a04597f2558f908b2db45368ff38f7e15
Dawid Senko <dawid.senko@jsql.it>
2019-02-02 00:09:00 +0100

    + porty, bazy

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 gradle bootJar
 sleep 1
-java -jar ./build/libs/jsql-api.jar --spring.datasource.url=jdbc:postgresql://46.41.138.32:5432/test_jsql_db
+java -jar ./build/libs/jsql-api.jar --spring.datasource.url=jdbc:postgresql://46.41.138.32:5432/jsql_test --server.port=8080
